25e948ab0ea2f5f0fc0d3c25261cb4c176df0c96
Pedro Trudes <pedro.jtrudes@gmail.com>
2022-02-02 02:18:52 -0300

    Eventos no teclado
    termino do curso de javascript do alura

--- curso08/main.js
+++ curso08/main.js
@@ -1,27 +1,38 @@
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
 
-function tocaSom(idElementoAudio) {
-
-    document.querySelector(idElementoAudio).play();
+  if (elemento != null && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    console.log("elemento não encontrado ou selector invalido");
+  }
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 
 //para
-for (let contador =0; contador < listaDeTeclas.length; contador++){
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; // template string
+  console.log(idAudio);
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; // template string
-    console.log(idAudio);
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
 
-    tecla.onclick = function () {
-        tocaSom(idAudio);
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
     }
+  };
 
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
-
 
 /*
 function tocaSomPom(){
